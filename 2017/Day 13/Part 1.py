01fe11b88066d38bc6e2ef978fb1d4107f415a43
Alexander Nathanail <adnathanail@hotmail.co.uk>
2018-11-29 23:41:15 +0000

    Rewrote day 13 Part 2 to use dictionary comprehension Part 1 to use part 2

--- 2017/Day 13/Part 1.py
+++ 2017/Day 13/Part 1.py
@@ -1,35 +1,10 @@
 from aocd import get_data
+import itertools
+
 inp = get_data(day=13).split("\n")
+fw = {int(row.split(": ")[0]):int(row.split(": ")[1]) for row in inp}
 
-fw = {}
-for row in inp:
-  d,r = row.split(": ") # Depth, Range
-  fw[d] = ['' for x in range(int(r)+1)]
-  fw[d][0] = 'S'
-  fw[d][-1] = 'D'
+def score(o):
+  return [i*fw[i] for i in fw if (i+o)%(2*fw[i]-2) == 0]
 
-def move_scanners(fw):
-  for k in list(fw):
-    i = fw[k].index('S')
-    if fw[k][-1] == 'D':
-      newi = i + 1
-      if newi >= (len(fw[k]) - 1):
-        newi -= 2
-        fw[k][-1] = 'U'
-    elif fw[k][-1] == 'U':
-      newi = i - 1
-      if newi <= -1:
-        newi += 2
-        fw[k][-1] = 'D'
-    fw[k][i] = ''
-    fw[k][newi] = 'S'
-  return fw
-
-x = 0
-s = 0 # Severity total
-while x <= max([int(x) for x in list(fw)]):
-  if str(x) in fw and fw[str(x)][0] == "S":
-    s += x * (len(fw[str(x)])-1)
-  fw = move_scanners(fw)
-  x += 1
-print(s)
+print(sum(score(0)))
--- 2017/Day 13/Part 2.py
+++ 2017/Day 13/Part 2.py
@@ -2,12 +2,10 @@ from aocd import get_data
 import itertools
 
 inp = get_data(day=13).split("\n")
-fw = {}
-for row in inp:
-  d,r = row.split(": ") # Depth, Range
-  fw[int(d)] = int(r)
+fw = {int(row.split(": ")[0]):int(row.split(": ")[1]) for row in inp}
 
 def score(o):
   return [i*fw[i] for i in fw if (i+o)%(2*fw[i]-2) == 0]
 
+print(sum(score(0)))
 print(next(d for d in itertools.count(0) if score(d) == []))
