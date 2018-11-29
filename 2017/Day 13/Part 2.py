a8f74388cf229054d5875e00e2a4a6d0cb0e7e62
Alexander Nathanail <adnathanail@hotmail.co.uk>
2018-11-29 23:41:31 +0000

    .

--- 2017/Day 13/Part 2.py
+++ 2017/Day 13/Part 2.py
@@ -7,5 +7,4 @@ fw = {int(row.split(": ")[0]):int(row.split(": ")[1]) for row in inp}
 def score(o):
   return [i*fw[i] for i in fw if (i+o)%(2*fw[i]-2) == 0]
 
-print(sum(score(0)))
 print(next(d for d in itertools.count(0) if score(d) == []))
