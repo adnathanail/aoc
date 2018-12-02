ecfc9c0e456761e00efe37ce0361713aa5a1dcb0
Alexander Nathanail <adnathanail@hotmail.co.uk>
2018-12-02 11:17:33 +0000

    Day 2 2018 Golfed

--- 2018/Day 2.py
+++ 2018/Day 2.py
@@ -5,25 +5,13 @@ start = time.time()
 
 # Part 1
 from collections import Counter
-twos = 0
-threes = 0
-for row in inp:
-  c = Counter(dict(Counter(row)).values())
-  twos += 1 if 2 in c else 0
-  threes += 1 if 3 in c else 0
-print(twos*threes)
+tt = [[1 if 2 in c else 0, 1 if 3 in c else 0] for c in (Counter(dict(Counter(row)).values()) for row in inp)]
+print(sum([z[0] for z in tt]) * sum([z[1] for z in tt]))
 
 # Part 2
-def nocd(s1,s2): # Numbers of characters different
-  x = 0
-  for i in range(len(s1)):
-    if s1[i] != s2[i]:
-      x += 1
-  return x
-for i in range(len(inp)):
-  for j in range(i+1,len(inp)):
-    if nocd(inp[i], inp[j]) == 1:
-      print(''.join([inp[i][x] for x in range(len(inp[i])) if inp[i][x] == inp[j][x]]))
+nocd = lambda s1,s2: sum([1 for i in range(len(s1)) if s1[i] != s2[i]]) # Numbers of characters different
+i,j = next(k[0] for k in ([[i,j] for j in range(i+1,len(inp)) if nocd(inp[i], inp[j]) == 1] for i in range(len(inp))) if k)
+print(''.join([inp[i][x] for x in range(len(inp[i])) if inp[i][x] == inp[j][x]]))
 
 end = time.time()
 print(end - start)
