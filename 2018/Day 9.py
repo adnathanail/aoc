64f7777fea0e8e7ab42d01b03d506d1b8d3d5239
Judith Nathanail <a142@judiths-macbook.lan>
2018-12-11 21:23:34 +0000

    Day 9 quicker

--- 2018/Day 9.py
+++ 2018/Day 9.py
@@ -4,25 +4,21 @@ inp = get_data(day=9, year=2018)
 start = time.time()
 
 import re
-nop, mm = [int(x) for x in re.match("(\d+) players; last marble is worth (\d+) points", inp).groups()] # Number of players, Max marble
+from collections import deque
+nop, mm = [int(x) for x in re.match(r"(\d+) players; last marble is worth (\d+) points", inp).groups()] # Number of players, Max marble
 
 def ms(nop, mm): # Max score
-  l = [0,1]
-  i = 1
+  l = deque([1,0])
   pss = [0 for _ in range(nop)] # Player scores
   cp = 0 # Current player
   for n in range(2,1+mm):
     if n % 23:
-      i += 2
-      if i > len(l):
-        i -= len(l)
-      l.insert(i,n)
+      l.append(n)
+      l.rotate(-1)
     else:
-      i -= 7
-      if i < 0:
-        i += len(l)
-      pss[cp] += n + l[i]
-      del l[i]
+      l.rotate(8)
+      pss[cp] += n + l.pop()
+      l.rotate(-2)
     cp += 1
     if cp >= len(pss):
       cp -= len(pss)
