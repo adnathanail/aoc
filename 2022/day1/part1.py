63588dc30d2b84d86e3444e7681f200b7115334f
Alex Nathanail <adnathanail@hotmail.co.uk>
2022-12-01 09:28:39 +0000

    2022 day 1

---
+++ 2022/day1/part1.py
@@ -0,0 +1,15 @@
+from aocd.models import Puzzle
+
+puzzle = Puzzle(year=2022, day=1)
+
+elf_totals = []
+current_elf_total = 0
+for line in puzzle.input_data.split("\n"):
+    if line == "":
+        elf_totals.append(current_elf_total)
+        current_elf_total = 0
+    else:
+        current_elf_total += int(line)
+elf_totals.append(current_elf_total)
+
+print(max(elf_totals))
