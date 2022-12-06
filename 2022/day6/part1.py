0bd390356e8d18eaca3b440de605fdd3c19fefd2
Alex Nathanail <adnathanail@hotmail.co.uk>
2022-12-06 07:18:53 +0000

    2022 day 6

---
+++ 2022/day6/part1.py
@@ -0,0 +1,8 @@
+from aocd.models import Puzzle
+
+puzzle = Puzzle(year=2022, day=6)
+
+for i in range(3, len(puzzle.input_data)):
+    if len(set(puzzle.input_data[i - 4:i])) == 4:
+        print(i)
+        break
---
+++ 2022/day6/part2.py
@@ -0,0 +1,10 @@
+from aocd.models import Puzzle
+
+puzzle = Puzzle(year=2022, day=6)
+
+NUM_UNIQUE = 14
+
+for i in range(NUM_UNIQUE - 1, len(puzzle.input_data)):
+    if len(set(puzzle.input_data[i - NUM_UNIQUE:i])) == NUM_UNIQUE:
+        print(i)
+        break
