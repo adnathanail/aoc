02b7231f0226c1e4675c5b2688898dafb5a8b149
Alex Nathanail <adnathanail@hotmail.co.uk>
2022-12-08 13:58:58 +0000

    2022 day 8 part 1 refactor (Not correct)

--- 2022/day8/part1.py
+++ 2022/day8/part1.py
@@ -1,4 +1,31 @@
+from typing import Generator
+
 from aocd.models import Puzzle  # type: ignore[import]
+
+
+def coord_list_generator(
+        row_min: int, row_max: int, row_step: int, col_min: int, col_max: int, col_step: int,
+        reverse: bool
+) -> Generator[list[tuple[int, int]], None, None]:
+    for i in range(row_min, row_max, row_step):
+        yield [((j, i) if reverse else (i, j)) for j in range(col_min, col_max, col_step)]
+
+
+def check_tree_visibility(
+        tree_height_grid: list[list[int]],
+        coords: Generator[list[tuple[int, int]], None, None]
+) -> set[tuple[int, int]]:
+    visible_trees = set()
+    for coord_list in coords:
+        last_tree_height = -1
+        for coord in coord_list:
+            i, j = coord
+            if tree_height_grid[i][j] < last_tree_height:
+                break
+            if tree_height_grid[i][j] > last_tree_height:
+                visible_trees.add((i, j))
+            last_tree_height = tree_height_grid[i][j]
+    return visible_trees
 
 
 def main() -> None:
@@ -9,40 +36,17 @@ def main() -> None:
 65332
 33549
 35390"""
-    grid = [[int(cell) for cell in row] for row in inp.split("\n")]
-    visible_trees = set()
-    for i in range(len(grid)):
-        last_tree_height = -1
-        for j in range(len(grid[i])):
-            if grid[i][j] < last_tree_height:
-                break
-            if grid[i][j] > last_tree_height:
-                visible_trees.add((i, j))
-            last_tree_height = grid[i][j]
-        last_tree_height = -1
-        for j in range(len(grid[i]) - 1, -1, -1):
-            if grid[i][j] < last_tree_height:
-                break
-            if grid[i][j] > last_tree_height:
-                visible_trees.add((i, j))
-            last_tree_height = grid[i][j]
-    # print(visible_trees)
-    # print(len(visible_trees))
-    for j in range(len(grid[0])):
-        last_tree_height = -1
-        for i in range(len(grid)):
-            if grid[i][j] < last_tree_height:
-                break
-            if grid[i][j] > last_tree_height:
-                visible_trees.add((i, j))
-            last_tree_height = grid[i][j]
-        last_tree_height = -1
-        for i in range(len(grid) - 1, -1, -1):
-            if grid[i][j] < last_tree_height:
-                break
-            if grid[i][j] > last_tree_height:
-                visible_trees.add((i, j))
-            last_tree_height = grid[i][j]
+    grid: list[list[int]] = [[int(cell) for cell in row] for row in inp.split("\n")]
+    coord_generators = [
+        coord_list_generator(0, len(grid), 1, 0, len(grid[0]), 1, False),
+        coord_list_generator(0, len(grid), 1, len(grid[0]) - 1, -1, -1, False),
+        coord_list_generator(0, len(grid), 1, 0, len(grid[0]), 1, True),
+        coord_list_generator(0, len(grid), 1, len(grid[0]) - 1, -1, -1, True)
+    ]
+    visible_trees: set[tuple[int, int]] = set()
+    for cg in coord_generators:
+        visible_trees = visible_trees.union(check_tree_visibility(grid, cg))
+        # print(visible_trees)
     print(len(visible_trees))
 
 
