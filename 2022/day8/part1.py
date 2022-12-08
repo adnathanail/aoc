dd9fb6bdbd8d57cc179e7edcb19636c61d78f767
Alex Nathanail <adnathanail@hotmail.co.uk>
2022-12-08 15:45:13 +0000

    2022 day 8 part 2 working!

--- 2022/day8/part1.py
+++ 2022/day8/part1.py
@@ -2,8 +2,6 @@ from typing import Generator
 
 from aocd.models import Puzzle  # type: ignore[import]
 
-from utils import *
-
 
 def coord_list_generator(
         row_min: int, row_max: int, row_step: int, col_min: int, col_max: int, col_step: int,
@@ -11,6 +9,21 @@ def coord_list_generator(
 ) -> Generator[list[tuple[int, int]], None, None]:
     for i in range(row_min, row_max, row_step):
         yield [((j, i) if reverse else (i, j)) for j in range(col_min, col_max, col_step)]
+
+
+def check_tree_visibility(
+        tree_height_grid: list[list[int]],
+        coords: Generator[list[tuple[int, int]], None, None]
+) -> set[tuple[int, int]]:
+    visible_trees = set()
+    for coord_list in coords:
+        last_highest_tree_height = -1
+        for coord in coord_list:
+            i, j = coord
+            if tree_height_grid[i][j] > last_highest_tree_height:
+                visible_trees.add(coord)
+                last_highest_tree_height = tree_height_grid[i][j]
+    return visible_trees
 
 
 def main() -> None:
@@ -24,8 +37,7 @@ def main() -> None:
     ]
     visible_trees: set[tuple[int, int]] = set()
     for cg in coord_generators:
-        for coords in cg:
-            visible_trees = visible_trees.union(check_tree_visibility(grid, coords))
+        visible_trees = visible_trees.union(check_tree_visibility(grid, cg))
     print(len(visible_trees))
 
 
