80c385c714177c45236f9fde4a2e4d6d3164eaee
Alex Nathanail <adnathanail@hotmail.co.uk>
2022-12-12 13:53:19 +0000

    2022 day 9 part 2 working!
    The new type of motion referred to in part 2 is that a pulling knot can move in 2 dimensions at once (as opposed to just U/D/L/R)

--- 2022/day9/part1.py
+++ 2022/day9/part1.py
@@ -1,31 +1,6 @@
-from dataclasses import dataclass
-from typing import Generator
-
 from aocd.models import Puzzle  # type: ignore[import]
 
-
-@dataclass(frozen=True)
-class Pos:
-    x: int
-    y: int
-
-
-def get_head_positions(puzzle_input: str) -> Generator[Pos, None, None]:
-    h_pos = Pos(0, 0)
-    for line in puzzle_input.split("\n"):
-        direction, distance_str = line.split(" ")
-        distance = int(distance_str)
-        for _ in range(distance):
-            match direction:
-                case "U":
-                    h_pos = Pos(h_pos.x, h_pos.y + 1)
-                case "D":
-                    h_pos = Pos(h_pos.x, h_pos.y - 1)
-                case "L":
-                    h_pos = Pos(h_pos.x - 1, h_pos.y)
-                case "R":
-                    h_pos = Pos(h_pos.x + 1, h_pos.y)
-            yield h_pos
+from utils import Pos, get_head_positions
 
 
 def main() -> None:
--- 2022/day9/part2.py
+++ 2022/day9/part2.py
@@ -1,56 +1,22 @@
-from dataclasses import dataclass
-from typing import Generator
-
 from aocd.models import Puzzle  # type: ignore[import]
 
-
-@dataclass(frozen=True)
-class Pos:
-    x: int
-    y: int
-
-
-def get_head_positions(puzzle_input: str) -> Generator[Pos, None, None]:
-    h_pos = Pos(0, 0)
-    for line in puzzle_input.split("\n"):
-        direction, distance_str = line.split(" ")
-        distance = int(distance_str)
-        for _ in range(distance):
-            match direction:
-                case "U":
-                    h_pos = Pos(h_pos.x, h_pos.y + 1)
-                case "D":
-                    h_pos = Pos(h_pos.x, h_pos.y - 1)
-                case "L":
-                    h_pos = Pos(h_pos.x - 1, h_pos.y)
-                case "R":
-                    h_pos = Pos(h_pos.x + 1, h_pos.y)
-            yield h_pos
+from utils import Pos, get_head_positions
 
 
 def drag_knot(leader: Pos, follower: Pos) -> Pos:
-    if leader.y - follower.y > 1:
-        return Pos(leader.x, leader.y - 1)
-    elif leader.y - follower.y < -1:
-        return Pos(leader.x, leader.y + 1)
-    elif leader.x - follower.x > 1:
-        return Pos(leader.x - 1, leader.y)
-    elif leader.x - follower.x < -1:
-        return Pos(leader.x + 1, leader.y)
-    return follower
+    dx, dy = leader.x - follower.x, leader.y - follower.y
+    if abs(dx) == 2 and abs(dy) == 2:
+        return Pos(leader.x - (dx // 2), leader.y - (dy // 2))
+    elif abs(dx) == 2 or abs(dy) == 2:
+        altx = 1 if dx > 0 else -1 if dx < 0 else 0
+        alty = 1 if dy > 0 else -1 if dy < 0 else 0
+        return Pos(follower.x + altx, follower.y + alty)
+    else:
+        return follower
 
 
 def main() -> None:
     puzzle = Puzzle(year=2022, day=9)
-    inp = puzzle.input_data
-    inp = """R 4
-U 4
-L 3
-D 1
-R 4
-D 1
-L 5
-R 2"""
     t_positions = {Pos(0, 0)}
     knot_positions = {
         "1": Pos(0, 0),
@@ -64,7 +30,7 @@ R 2"""
         "9": Pos(0, 0),
     }
     tail_path = []
-    for h_pos in get_head_positions(inp):
+    for h_pos in get_head_positions(puzzle.input_data):
         knot_positions["1"] = drag_knot(h_pos, knot_positions["1"])
         knot_positions["2"] = drag_knot(knot_positions["1"], knot_positions["2"])
         knot_positions["3"] = drag_knot(knot_positions["2"], knot_positions["3"])
@@ -77,8 +43,24 @@ R 2"""
         if knot_positions["9"] not in t_positions:
             tail_path.append(knot_positions["9"])
         t_positions.add(knot_positions["9"])
+        # Print function for first test input
+        # for i in range(4, -1, -1):
+        #     for j in range(6):
+        #         ij_pos = Pos(j, i)
+        #         if h_pos == ij_pos:
+        #             print("H", end="")
+        #             continue
+        #         printed = False
+        #         for k in range(1, 9 + 1):
+        #             if knot_positions[str(k)] == ij_pos:
+        #                 print(k, end="")
+        #                 printed = True
+        #                 break
+        #         if not printed:
+        #             print(".", end="")
+        #     print()
+        # print()
     print(len(t_positions))
-    print(tail_path)
 
 
 if __name__ == "__main__":
