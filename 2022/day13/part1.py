f1533ce47a29b18d4c02eadf26ac9a6abfafced3
Alex Nathanail <adnathanail@hotmail.co.uk>
2022-12-20 18:18:39 +0000

    Day 13 part 1 V4 parse full input

--- 2022/day13/part1.py
+++ 2022/day13/part1.py
@@ -5,25 +5,6 @@ from aocd.models import Puzzle  # type: ignore[import]
 Packet = list[Union[int, "Packet"]]
 
 
-def get_line_pairs(inp: str) -> list[tuple[str, str]]:
-    out = []
-    lines = inp.split("\n")
-    last_line = ""
-    for i in range(len(lines)):
-        if i % 3 == 0:
-            last_line = lines[i]
-        elif i % 3 == 1:
-            out.append((last_line, lines[i]))
-        else:
-            last_line = ""
-    return out
-
-
-def parse_input(inp: str) -> list[tuple[Packet, Packet]]:
-    line_pairs = get_line_pairs(inp)
-    return line_pairs
-
-
 def parse_packet_int(inp: str, curr: int) -> tuple[int, int]:
     out_str = ""
     while inp[curr] not in [",", "]"]:
@@ -67,40 +48,51 @@ def parse_packet(inp: str) -> Packet:
     return parse_packet_list(inp.replace(" ", ""), 0, 0)[0]
 
 
+def get_line_pairs(inp: str) -> list[tuple[str, str]]:
+    out = []
+    lines = inp.split("\n")
+    last_line = ""
+    for i in range(len(lines)):
+        if i % 3 == 0:
+            last_line = lines[i]
+        elif i % 3 == 1:
+            out.append((last_line, lines[i]))
+        else:
+            last_line = ""
+    return out
+
+
+def parse_input(inp: str) -> list[tuple[Packet, Packet]]:
+    return [(parse_packet(pair[0]), parse_packet(pair[1])) for pair in get_line_pairs(inp)]
+
+
 def main() -> None:
-    puzzle = Puzzle(year=2022, day=13)
-    #     pairs: list[tuple[Packet, Packet]] = parse_input("""[1,1,3,1,1]
-    # [1,1,5,1,1]
-    #
-    # [[1],[2,3,4]]
-    # [[1],4]
-    #
-    # [9]
-    # [[8,7,6]]
-    #
-    # [[4,4],4,4]
-    # [[4,4],4,4,4]
-    #
-    # [7,7,7,7]
-    # [7,7,7]
-    #
-    # []
-    # [3]
-    #
-    # [[[]]]
-    # [[]]
-    #
-    # [1,[2,[3,[4,[5,6,7]]]],8,9]
-    # [1,[2,[3,[4,[5,6,0]]]],8,9]""")
-    #     for pair in pairs:
-    #         print(pair)
-    assert parse_packet("[]") == []
-    assert parse_packet("[[]]") == [[]]
-    assert parse_packet("[[],[]]") == [[], []]
-    assert parse_packet("[[], [[], []]]") == [[], [[], []]]
-    assert parse_packet("[[], [[], []]]") == [[], [[], []]]
-    assert parse_packet("[1]") == [1]
-    assert parse_packet("[[[],[1],[],[1,[[1]],7],[44,4],4,4]]") == [[[], [1], [], [1, [[1]], 7], [44, 4], 4, 4]]
+    # puzzle = Puzzle(year=2022, day=13)
+    pairs: list[tuple[Packet, Packet]] = parse_input("""[1,1,3,1,1]
+[1,1,5,1,1]
+
+[[1],[2,3,4]]
+[[1],4]
+
+[9]
+[[8,7,6]]
+
+[[4,4],4,4]
+[[4,4],4,4,4]
+
+[7,7,7,7]
+[7,7,7]
+
+[]
+[3]
+
+[[[]]]
+[[]]
+
+[1,[2,[3,[4,[5,6,7]]]],8,9]
+[1,[2,[3,[4,[5,6,0]]]],8,9]""")
+    for pair in pairs:
+        print(pair)
 
 
 if __name__ == "__main__":
