f8d732a4a98f788f01633949704433b5fbca1189
Alex Nathanail <adnathanail@hotmail.co.uk>
2022-12-20 22:12:22 +0000

    Make day 15 use new type classes

--- 2022/day15/part1.py
+++ 2022/day15/part1.py
@@ -2,14 +2,18 @@ import re
 
 from aocd.models import Puzzle  # type: ignore[import]
 
-from project_utils import get_from_grid, set_on_grid
+from project_utils import set_on_grid, Coord, StrGrid
 
 
-def get_sensors_and_beacons(inp: str) -> tuple[list[tuple[int, int]], list[tuple[int, int]]]:
+def get_sensors_and_beacons(
+    inp: str,
+) -> tuple[list[tuple[int, int]], list[tuple[int, int]]]:
     sensors = []
     beacons = []
 
-    patt = re.compile(r"Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)")
+    patt = re.compile(
+        r"Sensor at x=(-?\d+), y=(-?\d+): closest beacon is at x=(-?\d+), y=(-?\d+)"
+    )
     for row in inp.split("\n"):
         s_x, s_y, b_x, b_y = [int(x) for x in re.match(patt, row).groups()]
         sensors.append((s_x, s_y))
@@ -34,9 +38,17 @@ def get_min_max_x_y(coords: list[tuple[int, int]]):
     return min_x, max_x, min_y, max_y
 
 
-def get_grid(sensors: list[tuple[int, int]], beacons: list[tuple[int, int]], min_x: int, max_x: int, min_y: int,
-             max_y: int) -> list[list[str]]:
-    grid: list[list[str]] = [["." for _ in range(min_x, max_x + 1)] for _ in range(min_y, max_y + 1)]
+def get_grid(
+    sensors: list[Coord],
+    beacons: list[Coord],
+    min_x: int,
+    max_x: int,
+    min_y: int,
+    max_y: int,
+) -> StrGrid:
+    grid: list[list[str]] = [
+        ["." for _ in range(min_x, max_x + 1)] for _ in range(min_y, max_y + 1)
+    ]
 
     for s in sensors:
         set_on_grid(grid, (s[0], s[1]), "S", min_x, min_y)
@@ -48,7 +60,8 @@ def get_grid(sensors: list[tuple[int, int]], beacons: list[tuple[int, int]], min
 
 def main() -> None:
     puzzle = Puzzle(year=2022, day=15)
-    sensors, beacons = get_sensors_and_beacons("""Sensor at x=2, y=18: closest beacon is at x=-2, y=15
+    sensors, beacons = get_sensors_and_beacons(
+        """Sensor at x=2, y=18: closest beacon is at x=-2, y=15
 Sensor at x=9, y=16: closest beacon is at x=10, y=16
 Sensor at x=13, y=2: closest beacon is at x=15, y=3
 Sensor at x=12, y=14: closest beacon is at x=10, y=16
@@ -61,7 +74,8 @@ Sensor at x=20, y=14: closest beacon is at x=25, y=17
 Sensor at x=17, y=20: closest beacon is at x=21, y=22
 Sensor at x=16, y=7: closest beacon is at x=15, y=3
 Sensor at x=14, y=3: closest beacon is at x=15, y=3
-Sensor at x=20, y=1: closest beacon is at x=15, y=3""")
+Sensor at x=20, y=1: closest beacon is at x=15, y=3"""
+    )
     min_x, max_x, min_y, max_y = get_min_max_x_y(beacons + sensors)
     grid = get_grid(sensors, beacons, min_x, max_x, min_y, max_y)
 
