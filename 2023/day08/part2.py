a95cc28c7dc02ecb05b7ad0b3d3e4303fe5ea91a
Alex Nathanail <adnathanail@hotmail.co.uk>
2023-12-08 23:09:37 +0000

    2023 Day 8 Part 2 V2

--- 2023/day08/part2.py
+++ 2023/day08/part2.py
@@ -1,3 +1,5 @@
+from math import lcm
+
 from aocd.models import Puzzle
 
 puzzle = Puzzle(year=2023, day=8)
@@ -10,20 +12,23 @@ node_map = {}
 for line in inp_split[2:]:
     node_map[line[:3]] = (line[7:10], line[12:15])
 
-current_nodes = [n for n in node_map.keys() if n[2] == "A"]
-current_instruction_index = 0
-n = 0
-while set(node[2] for node in current_nodes) != {"Z"}:
-    for i in range(len(current_nodes)):
+
+def get_num_steps(current_node):
+    current_instruction_index = 0
+    n = 0
+    while current_node[2] != "Z":
         if instructions[current_instruction_index] == "L":
-            current_nodes[i] = node_map[current_nodes[i]][0]
+            current_node = node_map[current_node][0]
         elif instructions[current_instruction_index] == "R":
-            current_nodes[i] = node_map[current_nodes[i]][1]
+            current_node = node_map[current_node][1]
         else:
             raise Exception("Invalid character")
-    n += 1
-    current_instruction_index += 1
-    if current_instruction_index == len(instructions):
-        current_instruction_index = 0
+        n += 1
+        current_instruction_index += 1
+        if current_instruction_index == len(instructions):
+            current_instruction_index = 0
 
-print(n)
+    return n
+
+
+print(lcm(*[get_num_steps(n) for n in node_map.keys() if n[2] == "A"]))
