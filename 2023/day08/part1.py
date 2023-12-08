91edeb78c7beacbd0234c24e0c4ea789a808f5c8
Alex Nathanail <adnathanail@hotmail.co.uk>
2023-12-08 23:06:25 +0000

    2023 Day 8 Part 1

---
+++ 2023/day08/part1.py
@@ -0,0 +1,28 @@
+from aocd.models import Puzzle
+
+puzzle = Puzzle(year=2023, day=8)
+
+inp_split = puzzle.input_data.splitlines()
+
+instructions = inp_split[0]
+
+node_map = {}
+for line in inp_split[2:]:
+    node_map[line[:3]] = (line[7:10], line[12:15])
+
+current_node = "AAA"
+current_instruction_index = 0
+n = 0
+while current_node != "ZZZ":
+    if instructions[current_instruction_index] == "L":
+        current_node = node_map[current_node][0]
+    elif instructions[current_instruction_index] == "R":
+        current_node = node_map[current_node][1]
+    else:
+        raise Exception("Invalid character")
+    n += 1
+    current_instruction_index += 1
+    if current_instruction_index == len(instructions):
+        current_instruction_index = 0
+
+print(n)
