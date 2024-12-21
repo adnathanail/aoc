b225280bd72097e332ecd6b75b60145cc8060225
Alex Nathanail <adnathanail@hotmail.co.uk>
2024-12-21 16:26:27 +0000

    2024 Day 21 Part 1 V4 doesn't cross bad keys, all examples work, real codes answer too high

--- 2024/day21/part1.py
+++ 2024/day21/part1.py
@@ -32,19 +32,23 @@ def enter_code(key_poss, code):
     for char in code:
         char_loc = key_poss[char]
         x_delta, y_delta = char_loc[0] - prev_loc[0], char_loc[1] - prev_loc[1]
+        x_first_bad = False
         simulating_path_loc = prev_loc
         for _ in range(abs(x_delta)):
             simulating_path_loc = (simulating_path_loc[0] + (-1 if x_delta < 0 else 1), simulating_path_loc[1])
             if simulating_path_loc == key_poss[None]:
-                print(code, char)
-                raise Exception("Can't cross the blank key!")
-        for _ in range(abs(y_delta)):
-            simulating_path_loc = (simulating_path_loc[0], simulating_path_loc[1] + (-1 if y_delta < 0 else 1))
-            if simulating_path_loc == key_poss[None]:
-                print(code, char)
-                raise Exception("Can't cross the blank key!")
-        out += x_delta_to_chars(x_delta)
-        out += y_delta_to_chars(y_delta)
+                x_first_bad = True
+        # for _ in range(abs(y_delta)):
+        #     simulating_path_loc = (simulating_path_loc[0], simulating_path_loc[1] + (-1 if y_delta < 0 else 1))
+        #     if simulating_path_loc == key_poss[None]:
+        #         print(code, char)
+        #         raise Exception("Can't cross the blank key!")
+        if x_first_bad:
+            out += y_delta_to_chars(y_delta)
+            out += x_delta_to_chars(x_delta)
+        else:
+            out += x_delta_to_chars(x_delta)
+            out += y_delta_to_chars(y_delta)
         out += "A"
         prev_loc = char_loc
     return out
@@ -63,16 +67,36 @@ robot_key_pad = (
 )
 robot_key_pad_coord_lookup = generate_coord_lookup(robot_key_pad)
 
-# codes = ["029A", "980A", "179A", "456A", "379A"]
-codes = ["179A"]
+def check_instructions_dont_cross_bad_key(key_poss, code):
+    loc = key_poss["A"]
+    for char in code:
+        if char == ">":
+            loc = (loc[0] + 1, loc[1])
+        elif char == "v":
+            loc = (loc[0], loc[1] + 1)
+        elif char == "<":
+            loc = (loc[0] - 1, loc[1])
+        elif char == "^":
+            loc = (loc[0], loc[1] - 1)
+        if loc == key_poss[None]:
+            raise Exception("Can't cross the bad key")
+
+codes = ["029A", "980A", "179A", "456A", "379A"]
+tot = 0
 for code in codes:
+    # print(code)
     robot_1_instructions = enter_code(number_key_pad_coord_lookup, code)
-    print("R1", robot_1_instructions)
+    # print("R1", robot_1_instructions)
+    check_instructions_dont_cross_bad_key(number_key_pad_coord_lookup, robot_1_instructions)
     robot_2_instructions = enter_code(robot_key_pad_coord_lookup, robot_1_instructions)
-    print("R2", robot_2_instructions)
+    # print("R2", robot_2_instructions)
+    check_instructions_dont_cross_bad_key(robot_key_pad_coord_lookup, robot_2_instructions)
     robot_3_instructions = enter_code(robot_key_pad_coord_lookup, robot_2_instructions)
-    print(robot_3_instructions)
-    print(len(robot_3_instructions))
-    print(int(code[:-1]))
-    print(len(robot_3_instructions) * int(code[:-1]))
+    check_instructions_dont_cross_bad_key(robot_key_pad_coord_lookup, robot_3_instructions)
+    # print("R3", robot_3_instructions)
+    print(len(robot_3_instructions), int(code[:-1]), len(robot_3_instructions) * int(code[:-1]))
     print()
+    tot += len(robot_3_instructions) * int(code[:-1])
+
+print(tot)
+# 189174 too high
