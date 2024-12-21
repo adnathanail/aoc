4f173eb675eb78a4c0946e0e741573f3a61ccd78
Alex Nathanail <adnathanail@hotmail.co.uk>
2024-12-21 10:15:02 +0000

    Really all these changes have been to the part 1 solver

--- 2024/day20/part1.py
+++ 2024/day20/part1.py
@@ -1,11 +1,26 @@
+import time
 from aocd.models import Puzzle
-import networkx as nx
 
 puzzle = Puzzle(year=2024, day=20)
 input_data = puzzle.input_data
+# input_data = """###############
+# #...#...#.....#
+# #.#.#.#.#.###.#
+# #S#...#.#.#...#
+# #######.#.#.###
+# #######.#.#...#
+# #######.#.###.#
+# ###..E#...#...#
+# ###.#######.###
+# #...###...#...#
+# #.#####.#.###.#
+# #.#...#.#.#...#
+# #.#.#.#.#.#.###
+# #...#...#...###
+# ###############"""
 
 
-def generate_grid(inp):
+def generate_grid():
     input_lines = input_data.splitlines()
 
     grid_arr = []
@@ -29,82 +44,84 @@ def generate_grid(inp):
     return grid_arr, start_loc, end_loc
 
 
-def generate_graph(in_grid):
-    out = nx.Graph()
-    for i in range(len(in_grid)):
-        for j in range(len(in_grid[i])):
-            if grid[i][j] != ".":
-                continue
-            out.add_node((j, i))
-            dirs = [(i - 1, j), (i, j - 1), (i + 1, j), (i, j + 1)]
-            for newi, newj in dirs:
-                if 0 <= newi < len(in_grid) and 0 <= newj < len(in_grid[j]):
-                    if in_grid[newi][newj] == ".":
-                        out.add_node((newj, newi))
-                        out.add_edge((j, i), (newj, newi))
-
+def generate_path(start):
+    out = [start]
+    deltas = [(0, 1), (1, 0), (0, -1), (-1, 0)]
+    current_delta = None
+    for delta in deltas:
+        nnext = (start[0] + delta[0], start[1] + delta[1])
+        if grid[nnext[1]][nnext[0]] == ".":
+            current_delta = delta
+            break
+    if current_delta is None:
+        raise Exception("No path from start")
+    current = start
+    over = False
+    while not over:
+        nnext = (current[0] + delta[0], current[1] + delta[1])
+        if grid[nnext[1]][nnext[0]] == ".":
+            current = nnext
+            out.append(nnext)
+        else:
+            rotated_delta = (abs(current_delta[1]), abs(current_delta[0]))
+            deltas_to_try = [rotated_delta, (-rotated_delta[0], -rotated_delta[1])]
+            current_delta = None
+            for delta in deltas_to_try:
+                nnext = (current[0] + delta[0], current[1] + delta[1])
+                if grid[nnext[1]][nnext[0]] == ".":
+                    current_delta = delta
+                    break
+            if current_delta is None:
+                over = True
     return out
 
 
-def list_cheats(in_grid):
+def list_cheats():
     cheats = []
-    for i in range(len(in_grid)):
-        for j in range(len(in_grid[i])):
-            if in_grid[i][j] != ".":
+    for i in range(height):
+        for j in range(width):
+            if grid[i][j] != ".":
                 continue
-            potential_jump_dirs = [(-1, 0), (0, -1), (1, 0), (0, 1)]
+            no_jump = (j, i)
+            potential_jump_dirs = [(1, 0), (0, 1)]
             for pjd in potential_jump_dirs:
-                no_jump = (j, i)
                 one_jump = (j + pjd[0], i + pjd[1])
                 two_jump = (j + pjd[0] * 2, i + pjd[1] * 2)
-                three_jump = (j + pjd[0] * 3, i + pjd[1] * 3)
-                if (
-                    0 <= one_jump[0] < len(in_grid[i])
-                    and 0 <= one_jump[1] < len(in_grid)
-                    and 0 <= two_jump[0] < len(in_grid[i])
-                    and 0 <= two_jump[1] < len(in_grid)
-                ):
-                    if in_grid[one_jump[1]][one_jump[0]] == "#":
-                        if in_grid[two_jump[1]][two_jump[0]] == ".":
-                            if (no_jump, two_jump) not in cheats and (two_jump, no_jump) not in cheats:
-                                cheats.append((no_jump, two_jump))
-                        if 0 <= three_jump[0] < len(in_grid[i]) and 0 <= three_jump[1] < len(in_grid):
-                            if in_grid[two_jump[1]][two_jump[0]] == "#" and in_grid[three_jump[1]][three_jump[0]] == ".":
-                                if (no_jump, three_jump) not in cheats and (three_jump, no_jump) not in cheats:
-                                    cheats.append((no_jump, three_jump))
+                if 0 <= two_jump[0] < width and 0 <= two_jump[1] < height:
+                    if grid[one_jump[1]][one_jump[0]] == "#" and grid[two_jump[1]][two_jump[0]] == ".":
+                        if (no_jump, two_jump) not in cheats and (two_jump, no_jump) not in cheats:
+                            cheats.append((no_jump, two_jump))
     return cheats
 
 
+def manhattan(p1, p2):
+    return abs(p1[0] - p2[0]) + abs(p1[1] - p2[1])
+
+
+def cheat_time_saved(ch):
+    return abs(the_path.index(ch[0]) - the_path.index(ch[1])) - manhattan(ch[0], ch[1])
+
+
+start_time = time.time()
+
 print("Generating grid")
-grid, start, end = generate_grid(input_data)
-print("Generating graph")
-graph = generate_graph(grid)
+grid, start, end = generate_grid()
+height = len(grid)
+width = len(grid[0])
 
+print("Generating path")
+the_path = generate_path(start)
 
-def get_shortest_path_with_cheat(G, s, e, cheat):
-    edge_length = abs(cheat[0][0] - cheat[1][0]) + abs(cheat[0][1] - cheat[1][1])
-    G.add_edge(cheat[0], cheat[1], weight=edge_length)
-    out = nx.shortest_path_length(G, s, e, weight="weight")
-    G.remove_edge(cheat[0], cheat[1])
-    return out
+print("Finding cheats")
+cheats = list_cheats()
+print(len(cheats), "found")
 
-
-shortest_path_no_cheats = nx.shortest_path_length(graph, start, end)
-print("Shortest path no cheats", shortest_path_no_cheats)
-
-tallies = {}
-
-for cheat in list_cheats(grid):
-    time_saved = shortest_path_no_cheats - get_shortest_path_with_cheat(graph, start, end, cheat)
-    if time_saved not in tallies:
-        tallies[time_saved] = 0
-    tallies[time_saved] += 1
-
-print("Cheats tallied")
-
+print("Testing cheats")
 num_good_cheats = 0
-for time_saved in tallies:
-    if time_saved >= 100:
-        num_good_cheats += tallies[time_saved]
+for cheat in cheats:
+    if cheat_time_saved(cheat) >= 100:
+        num_good_cheats += 1
 
 print(num_good_cheats)
+
+print("Time taken", time.time() - start_time)
