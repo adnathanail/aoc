cd9f7d6aa4a0010c75314a69f9312dee9a661c1d
Alex Nathanail <7809723+adnathanail@users.noreply.github.com>
2025-12-02 00:11:35 +0100

    2025 day 1 part 2 initial

--- 2025/src/bin/01.rs
+++ 2025/src/bin/01.rs
@@ -4,14 +4,14 @@ pub fn part_one(input: &str) -> Option<u64> {
     let mut curr_pos: i32 = 50;
     let mut num_zeroes: u64 = 0;
     for line in input.lines() {
-        let n: i32 = line[1..].parse().expect("msg");
+        let n: i32 = line[1..].parse().expect("Should be integer");
         if line.chars().next().unwrap() == 'L' {
             curr_pos -= n;
         } else {
             curr_pos += n;
         }
         curr_pos = curr_pos % 100;
-        println!("{}", curr_pos);
+
         if curr_pos == 0 {
             num_zeroes += 1;
         }
@@ -19,8 +19,23 @@ pub fn part_one(input: &str) -> Option<u64> {
     Some(num_zeroes)
 }
 
-pub fn part_two(_input: &str) -> Option<u64> {
-    None
+pub fn part_two(input: &str) -> Option<u64> {
+    let mut curr_pos: i32 = 50;
+    let mut num_zeroes: u64 = 0;
+    for line in input.lines() {
+        let n: i32 = line[1..].parse().expect("Should be integer");
+        if line.chars().next().unwrap() == 'L' {
+            curr_pos -= n;
+        } else {
+            curr_pos += n;
+        }
+        curr_pos = curr_pos % 100;
+
+        if curr_pos == 0 {
+            num_zeroes += 1;
+        }
+    }
+    Some(num_zeroes)
 }
 
 #[cfg(test)]
@@ -36,6 +51,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(6));
     }
 }
