e0f319f82a9f3a6d0743b6fc5f81717ad4eed1b8
Alex Nathanail <7809723+adnathanail@users.noreply.github.com>
2025-12-02 01:03:34 +0100

    2025 day 1 part 2

--- 2025/src/bin/01.rs
+++ 2025/src/bin/01.rs
@@ -22,18 +22,21 @@ pub fn part_one(input: &str) -> Option<u64> {
 pub fn part_two(input: &str) -> Option<u64> {
     let mut curr_pos: i32 = 50;
     let mut num_zeroes: u64 = 0;
+    let mut minus: bool;
     for line in input.lines() {
         let n: i32 = line[1..].parse().expect("Should be integer");
-        if line.chars().next().unwrap() == 'L' {
-            curr_pos -= n;
-        } else {
-            curr_pos += n;
+        minus = line.chars().next().unwrap() == 'L';
+        for _ in 0..n {
+            if minus {
+                curr_pos -= 1;
+            } else {
+                curr_pos += 1;
+            }
+            if curr_pos % 100 == 0 {
+                num_zeroes += 1;
+            }
         }
-        curr_pos = curr_pos % 100;
-
-        if curr_pos == 0 {
-            num_zeroes += 1;
-        }
+        
     }
     Some(num_zeroes)
 }
