43405e3d849c3c04707a9d143e474b82ff519497
Alex Nathanail <7809723+adnathanail@users.noreply.github.com>
2025-12-06 00:13:43 +0100

    2025 day 5 part 2 checkpoint

--- 2025/day05/part2.py
+++ 2025/day05/part2.py
@@ -2,7 +2,7 @@ from aocd.models import Puzzle
 
 puzzle = Puzzle(year=2025, day=5)
 inp = puzzle.input_data
-inp = puzzle.examples[0].input_data
+# inp = puzzle.examples[0].input_data
 
 ranges_str, numbers_str = inp.split("\n\n")
 
@@ -20,20 +20,26 @@ def find_ranges_indexes_to_merge():
         for j in range(i + 1, len(ranges)):
             if merge_ranges(*ranges[i], *ranges[j]):
                 return (i, j)
+    return None
 
-itm = find_ranges_indexes_to_merge()
+print(ranges)
+while True:
+    itm = find_ranges_indexes_to_merge()
+    if itm is None:
+        break
 
-rtm1 = ranges[itm[0]]
-rtm2 = ranges[itm[1]]
-# print(rtm1, rtm2)
-# print(min(rtm1[0], rtm2[0]), max(rtm1[1], rtm2[1]))
+    rtm1 = ranges[itm[0]]
+    rtm2 = ranges[itm[1]]
 
-new_ranges = [(min(rtm1[0], rtm2[0]), max(rtm1[1], rtm2[1]))]
-for i in range(len(ranges)):
-    if i not in itm:
-        new_ranges.append(ranges[i])
+    new_ranges = [(min(rtm1[0], rtm2[0]), max(rtm1[1], rtm2[1]))]
+    for i in range(len(ranges)):
+        if i not in itm:
+            new_ranges.append(ranges[i])
+    ranges = new_ranges
+    print(ranges)
 
-print(new_ranges)
+fresh_ingredients = 0
+for rr in ranges:
+    fresh_ingredients += rr[1] - rr[0] + 1
 
-# print(merge_ranges(10,14,12,18))
-# print(merge_ranges(10,20,14,16))
+print(fresh_ingredients)
