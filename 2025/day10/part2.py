c64c9e346d5559961d4203b105c4f82d52ef9f5d
Alex Nathanail <7809723+adnathanail@users.noreply.github.com>
2025-12-13 13:35:43 +0100

    2025 day 10 optimise with priority queue

--- 2025/day10/part2.py
+++ 2025/day10/part2.py
@@ -1,11 +1,12 @@
-from itertools import chain, combinations
+from heapq import heappush, heappop
 
 from aocd.models import Puzzle
 
 puzzle = Puzzle(year=2025, day=10)
 inp = puzzle.examples[0].input_data
 # inp = puzzle.input_data
-inp = "[#....#...#] (1,2,3,4,6,7,8) (4,9) (2,3,6,7,9) (0,3,7,8) (0,3,5,8) (0,4,5,6) (4,5,6,8) (1,2,4,6,7,9) {29,3,15,31,45,32,44,31,38,28}"
+# inp = "[#....#...#] (1,2,3,4,6,7,8) (4,9) (2,3,6,7,9) (0,3,7,8) (0,3,5,8) (0,4,5,6) (4,5,6,8) (1,2,4,6,7,9) {29,3,15,31,45,32,44,31,38,28}"
+inp = "[##.#] (0,1,3) (0,2) {25,12,13,12}"
 
 # Parse machine data
 machines = []
@@ -22,32 +23,53 @@ def button_wiring_to_tuple(buttons, num_lights):
 
 
 def add_tuples(t1, t2):
+    """ Sum of 2 equal-length tuples, elementwise """
     return tuple(sum(t) for t in zip(t1, t2))
 
-def compare_tuples(t1, t2):
-    for i in range(len(t1)):
-        if t1[i] > t2[i]:
+def compare_tuples(t_smaller, t_larger):
+    """ Check that every element of 1 tuple is less than or equal to the other """
+    for i in range(len(t_smaller)):
+        if t_smaller[i] > t_larger[i]:
             return False
     return True
 
+def tuple_distance(t_smaller, t_larger):
+    """ Get the total distance between each pair of elements across 2 tuples """
+    out = 0
+    for i in range(len(t_smaller)):
+        out += t_larger[i] - t_smaller[i]
+    return out
+
 def find_smallest_presses(button_wirings, desired_joltages):
     start_state = tuple(0 for _ in desired_joltages)
-    states_to_visit = set([start_state])
-    states = {start_state: 0}
     button_tuples = [button_wiring_to_tuple(button, len(desired_joltages)) for button in button_wirings]
+    # Priority queue, ordered by "manhattan-distance" between state and desired_joltages
+    states_to_visit = []
+    heappush(states_to_visit, (0, start_state))
+    # Dictionary of states to number of buttons to reach that state
+    states = {start_state: 0}
     while desired_joltages not in states:
-        new_states_to_visit = set()
-        for state in states_to_visit:
-            for bt in button_tuples:
-                potential_state = add_tuples(state, bt)
-                if compare_tuples(potential_state, desired_joltages):
-                    if potential_state not in states:
+        # Get next state to explore
+        _priority, state = heappop(states_to_visit)
+        for bt in button_tuples:
+            # Try all button press options
+            potential_state = add_tuples(state, bt)
+            # If the button press hasn't knocked us over desired_joltages
+            if compare_tuples(potential_state, desired_joltages):
+                # If we've already seen this state
+                if potential_state in states:
+                    # If the new route to it was better
+                    if states[potential_state] > (states[state] + 1):
+                        # Update new shorter option
                         states[potential_state] = states[state] + 1
-                        new_states_to_visit.add(potential_state)
+                # If we've never seen it before
+                else:
+                    # Add state to dictionary
+                    states[potential_state] = states[state] + 1
+                    # Add state to priority queue, ordered by distance to end state
+                    heappush(states_to_visit, (tuple_distance(potential_state, desired_joltages), potential_state))
 
-        states_to_visit = new_states_to_visit
     return states[desired_joltages]
-
 
 num_button_presses = 0
 for machine in machines:
