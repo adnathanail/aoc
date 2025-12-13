5a1dcf331253ab45f3783be493a52d8635e7191e
Alex Nathanail <7809723+adnathanail@users.noreply.github.com>
2025-12-13 21:52:40 +0100

    2025 day 12 part 1 add caching

--- 2025/day12/part1.py
+++ 2025/day12/part1.py
@@ -1,3 +1,4 @@
+from functools import lru_cache
 from functools import cache
 from aocd.models import Puzzle
 
@@ -45,6 +46,7 @@ def rotate_present_right(present_coords):
 def flip_present_horizontally(present_coords):
     return frozenset((2-coord[0], coord[1]) for coord in present_coords)
 
+@cache
 def get_all_versions_of_present(present_coords):
     flipped_present = flip_present_horizontally(present_coords)
     out = set([present_coords, flipped_present])
@@ -56,32 +58,33 @@ def get_all_versions_of_present(present_coords):
         out.add(flipped_present)
     return out
 
-press, regs = process_input(inp)
+PRESS, REGS = process_input(inp)
 
 @cache
 def offset_present(present_coords, x_offset, y_offset):
     return frozenset((coord[0] + x_offset, coord[1] + y_offset) for coord in present_coords)
 
 
-def attempt_placement(current_placement, presents, region_width, region_height, present_ids_to_place):
+@lru_cache
+def attempt_placement(current_placement, region_width, region_height, present_ids_to_place):
     if not present_ids_to_place:
         return current_placement
-    for present_coords in get_all_versions_of_present(presents[present_ids_to_place[0]]):
+    for present_coords in get_all_versions_of_present(PRESS[present_ids_to_place[0]]):
         for x_offset in range(0, region_width - PRESENT_WIDTH_HEIGHT + 1):
             for y_offset in range(0, region_height - PRESENT_WIDTH_HEIGHT + 1):
                 present_to_place = offset_present(present_coords, x_offset, y_offset)
-                if current_placement.intersection(present_to_place) == set():
-                    maybe_working_arrangement = attempt_placement(current_placement.union(present_to_place), presents, region_width, region_height, present_ids_to_place[1:])
+                if present_to_place - current_placement == present_to_place:
+                    maybe_working_arrangement = attempt_placement(current_placement.union(present_to_place), region_width, region_height, present_ids_to_place[1:])
                     if maybe_working_arrangement:
                         return maybe_working_arrangement
     return False
 
 
-for region in regs[:-1]:
+for region in REGS:
     wh, pts = region
     pids_to_place_this_region = []
     for i in range(len(pts)):
         for _ in range(pts[i]):
             pids_to_place_this_region.append(i)
-    if placement := attempt_placement(set(), press, wh[0], wh[1], pids_to_place_this_region):
+    if placement := attempt_placement(frozenset(), wh[0], wh[1], tuple(pids_to_place_this_region)):
         print_grid(placement, width=wh[0], height=wh[1])
