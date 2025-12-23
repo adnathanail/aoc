f02155a14aadd682c22cab6366bb38ef066c3144
Alex Nathanail <7809723+adnathanail@users.noreply.github.com>
2025-12-23 01:48:48 +0000

    2025 Rust day 3 part 2

--- _rust_abandoned_2025/src/bin/03.rs
+++ _rust_abandoned_2025/src/bin/03.rs
@@ -36,8 +36,28 @@ pub fn part_one(input: &str) -> Option<u64> {
     Some(total_joltage)
 }
 
+fn get_largest_n_digit_substring(num: &str, n: u64) -> u64 {
+    let mut left_offset = 0;
+    let mut right_offset = n as usize;
+    let mut out = 0;
+
+    // i is used to get powers of 10, to construct the number, so we go from 12 - 0
+    for i in (0..n).rev() {
+        right_offset -= 1;
+        let (digit, index) =
+            get_largest_digit_in_number(&num[left_offset..num.len() - right_offset]).unwrap();
+        out += digit * 10_u64.pow(i as u32);
+        left_offset += index + 1;
+    }
+    out
+}
+
 pub fn part_two(input: &str) -> Option<u64> {
-    None
+    let mut total_joltage = 0;
+    for row in input.strip_suffix("\n").unwrap().split("\n") {
+        total_joltage += get_largest_n_digit_substring(row, 12);
+    }
+    Some(total_joltage)
 }
 
 #[cfg(test)]
@@ -53,6 +73,6 @@ mod tests {
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(3121910778619));
     }
 }
