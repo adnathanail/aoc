106d6bae71163b1fb1d097c20a1490001d28baab
Alex Nathanail <7809723+adnathanail@users.noreply.github.com>
2025-12-24 20:14:14 +0000

    Rust 2025 day 8 part 1 test passes, too slow

--- _rust_abandoned_2025/src/bin/08.rs
+++ _rust_abandoned_2025/src/bin/08.rs
@@ -1,7 +1,74 @@
+use disjoint_sets::UnionFind;
+use std::collections::{HashMap, HashSet};
+use std::hash::Hash;
 advent_of_code::solution!(8);
 
+fn process_input(input: &str) -> Vec<Vec<i64>> {
+    input
+        .strip_suffix("\n")
+        .unwrap()
+        .split("\n")
+        .map(|row| row.split(",").map(|x| x.parse().unwrap()).collect())
+        .collect()
+}
+
+fn euc_dist(a: &Vec<i64>, b: &Vec<i64>) -> f32 {
+    (((a[0] - b[0]).pow(2) + (a[1] - b[1]).pow(2) + (a[2] - b[2]).pow(2)) as f32).sqrt()
+}
+
+fn tally_vec<T: Hash + Eq + Copy>(inp: &Vec<T>) -> HashMap<T, u64> {
+    let mut out: HashMap<T, u64> = HashMap::new();
+    for val in inp {
+        out.insert(*val, out.get(val).unwrap_or(&0) + 1);
+    }
+    out
+}
+
 pub fn part_one(input: &str) -> Option<u64> {
-    None
+    let junction_boxes = process_input(input);
+    let mut distance_lookup: HashMap<usize, HashMap<usize, f32>> = HashMap::new();
+    for i in 0..(junction_boxes.len() - 1) {
+        let mut i_distances: HashMap<usize, f32> = HashMap::new();
+        for j in (i + 1)..junction_boxes.len() {
+            i_distances.insert(j, euc_dist(&junction_boxes[i], &junction_boxes[j]));
+        }
+        distance_lookup.insert(i, i_distances);
+    }
+    // println!("{:?}", junction_boxes);
+    // println!("{:?}", distance_lookup);
+    let mut connected_components: UnionFind<usize> = UnionFind::new(junction_boxes.len());
+    let mut connections: Vec<(usize, usize)> = vec![];
+    for i in 0..10 {
+        println!("{}", i);
+        let mut closest_points: Option<(usize, usize)> = None;
+        let mut shortest_distance: f32 = f32::MAX;
+        for i in distance_lookup.keys() {
+            for j in distance_lookup[i].keys() {
+                // println!("{} {} {}", i, j, distance_lookup[i][j]);
+                if distance_lookup[i][j] < shortest_distance && !connections.contains(&(*i, *j)) {
+                    shortest_distance = distance_lookup[i][j];
+                    closest_points = Some((*i, *j));
+                }
+            }
+        }
+        // println!("{:?} {}", closest_points, shortest_distance);
+        connections.push(closest_points.unwrap());
+        connected_components.union(closest_points.unwrap().0, closest_points.unwrap().1);
+    }
+    // let x: HashSet<&usize> = HashSet::from_iter(connected_components.to_vec());
+    // println!("{:?}", connected_components);
+    // println!("{:?}", connected_components.to_vec());
+    let mut tallies: Vec<u64> = tally_vec(&connected_components.to_vec())
+        .values()
+        .map(|x| *x)
+        .collect();
+    // println!("{:?}", tallies);
+    tallies.sort();
+    let blah: Vec<u64> = tallies.iter().rev().take(3).map(|x| *x).collect();
+    // println!("{:?}", );
+    // println!("{:?}", x);
+    // println!("{:?}", connections);
+    Some(blah.iter().product::<u64>())
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
@@ -15,12 +82,12 @@ mod tests {
     #[test]
     fn test_part_one() {
         let result = part_one(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(40));
     }
 
     #[test]
     fn test_part_two() {
         let result = part_two(&advent_of_code::template::read_file("examples", DAY));
-        assert_eq!(result, None);
+        assert_eq!(result, Some(25272));
     }
 }
