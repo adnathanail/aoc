53f24e4c60173fd37c232c5190f408a0884fe7a3
Alex Nathanail <7809723+adnathanail@users.noreply.github.com>
2025-12-25 12:18:34 +0000

    2025 Rust day 11 part 1 switch to using numbers instead of strings

--- _rust_abandoned_2025/src/bin/11.rs
+++ _rust_abandoned_2025/src/bin/11.rs
@@ -1,33 +1,71 @@
 use std::collections::HashMap;
-
+// use std::sync::{LazyLock, Mutex};
 advent_of_code::solution!(11);
 
-type Cables<'a> = HashMap<&'a str, Vec<&'a str>>;
+type Cables = HashMap<usize, Vec<usize>>;
+type CableNames<'a> = Vec<&'a str>;
 
-fn get_cables<'a>(input: &'a str) -> Cables<'a> {
-    let mut out = HashMap::new();
-    for row in input.strip_suffix("\n").unwrap().split("\n") {
-        let row_split: Vec<&str> = row.split(": ").collect();
-        out.insert(row_split[0], row_split[1].split(" ").collect());
-    }
-    out
+fn get_cable_index(cable_names: &Vec<&str>, val: &str) -> usize {
+    println!("{}", val);
+    cable_names.iter().position(|x| *x == val).unwrap()
 }
 
-fn follow_cables(cables: &Cables, start_cable: &str, end_cable: &str) -> u64 {
+fn get_cables<'a>(input: &'a str) -> (CableNames<'a>, Cables) {
+    let mut out = HashMap::new();
+    let mut cable_names: Vec<&str> = input
+        .strip_suffix("\n")
+        .unwrap()
+        .split("\n")
+        .map(|x| x.split(": ").collect::<Vec<&str>>()[0])
+        .collect();
+    cable_names.push("out");
+    for row in input.strip_suffix("\n").unwrap().split("\n") {
+        let row_split: Vec<&str> = row.split(": ").collect();
+        out.insert(
+            get_cable_index(&cable_names, row_split[0]),
+            row_split[1]
+                .split(" ")
+                .map(|x| get_cable_index(&cable_names, x))
+                .collect::<Vec<usize>>(),
+        );
+    }
+    (cable_names, out)
+}
+
+// static FOLLOW_CABLES_CACHE: LazyLock<Mutex<HashMap<(&str, &str), u64>>> =
+//     LazyLock::new(|| Mutex::new(HashMap::new()));
+
+fn follow_cables(cables: &Cables, start_cable: &usize, end_cable: &usize) -> u64 {
+    // if let Some(&result) = FOLLOW_CABLES_CACHE
+    //     .lock()
+    //     .unwrap()
+    //     .get(&(start_cable, end_cable))
+    // {
+    //     return result;
+    // }
+
     let mut out = 0;
     for c in cables[start_cable].iter() {
-        if *c == end_cable {
+        if c == end_cable {
             out += 1;
         } else {
             out += follow_cables(cables, c, end_cable)
         }
     }
+    // FOLLOW_CABLES_CACHE
+    //     .lock()
+    //     .unwrap()
+    //     .insert((start_cable, end_cable), out);
     out
 }
 
 pub fn part_one(input: &str) -> Option<u64> {
-    let cables = get_cables(input);
-    Some(follow_cables(&cables, "you", "out"))
+    let (cable_names, cables) = get_cables(input);
+    Some(follow_cables(
+        &cables,
+        &get_cable_index(&cable_names, "you"),
+        &get_cable_index(&cable_names, "out"),
+    ))
 }
 
 pub fn part_two(input: &str) -> Option<u64> {
