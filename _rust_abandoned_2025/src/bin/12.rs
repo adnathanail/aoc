2b8a91c78ceeb800506f30ef9cd7a2953a6f414f
Alex Nathanail <7809723+adnathanail@users.noreply.github.com>
2025-12-26 18:20:53 +0000

    2025 Rust day 12 initial

---
+++ _rust_abandoned_2025/src/bin/12.rs
@@ -0,0 +1,16 @@
+advent_of_code::solution!(12, 1);
+
+pub fn part_one(input: &str) -> Option<u64> {
+    None
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_part_one() {
+        let result = part_one(&advent_of_code::template::read_file("examples", DAY));
+        assert_eq!(result, Some(2));
+    }
+}
