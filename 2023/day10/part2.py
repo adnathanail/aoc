990161ca95b608d11c3ef91e8100d627d37bc360
Alex Nathanail <7809723+adnathanail@users.noreply.github.com>
2026-01-14 13:14:12 +0100

    2023 day 10 part 2 cp 1

--- 2023/day10/part2.py
+++ 2023/day10/part2.py
@@ -2,6 +2,25 @@ from aocd.models import Puzzle
 
 puzzle = Puzzle(year=2023, day=10)
 inp = puzzle.input_data
+# inp = """.....
+# .S-7.
+# .|.|.
+# .L-J.
+# ....."""
+inp = """..F7.
+.FJ|.
+SJ.L7
+|F--J
+LJ..."""
+
+pipe_char_delta_lookup = {
+    "|": ((-1, 0), (1, 0)),
+    "-": ((0, -1), (0, 1)),
+    "L": ((0, 1), (-1, 0)),
+    "J": ((0, -1), (-1, 0)),
+    "7": ((0, -1), (1, 0)),
+    "F": ((0, 1), (1, 0)),
+}
 
 def get_grid(inp_str):
     out = []
@@ -19,25 +38,17 @@ def find_start(grid):
 
 def get_potential_nexts(grid, loc):
     char = grid[loc[0]][loc[1]]
-
-    if char == "|":
-        return [(loc[0] - 1, loc[1]), (loc[0] + 1, loc[1])]
-    elif char == "-":
-        return [(loc[0], loc[1] - 1), (loc[0], loc[1] + 1)]
-    elif char == "L":
-        return [(loc[0], loc[1] + 1), (loc[0] - 1, loc[1])]
-    elif char == "J":
-        return [(loc[0], loc[1] - 1), (loc[0] - 1, loc[1])]
-    elif char == "7":
-        return [(loc[0], loc[1] - 1), (loc[0] + 1, loc[1])]
-    elif char == "F":
-        return [(loc[0], loc[1] + 1), (loc[0] + 1, loc[1])]
-
-    return []
+    if char == ".":
+        return []
+    pipe_char_delts = pipe_char_delta_lookup[char]
+    return [(loc[0] + delt[0], loc[1] + delt[1]) for delt in pipe_char_delts]
 
 
 def get_next_location(grid, loc, prev_loc):
     potential_nexts = get_potential_nexts(grid, loc)
+
+    if prev_loc is None:  # Pick random direction, if we have no previous_loc
+        return potential_nexts[0]
 
     potential_nexts.remove(prev_loc)
 
@@ -47,18 +58,19 @@ def get_next_location(grid, loc, prev_loc):
     return potential_nexts[0]
 
 
-def get_next_after_start(grid, start):
+def detect_start_type(grid, start):
+    available_dirs = []
     for delt in [(0, 1), (0, -1), (1, 0), (-1, 0)]:
         potential_next = (start[0] + delt[0], start[1] + delt[1])
         potential_next_potential_prevs = get_potential_nexts(grid, potential_next)
         if start in potential_next_potential_prevs:
-            return potential_next
+            available_dirs.append(delt)
+    return available_dirs
 
-
-def get_path(grid):
-    prev = find_start(grid)
-    curr = get_next_after_start(grid, prev)
-    while grid[curr[0]][curr[1]] != "S":
+def get_path(grid, start):
+    prev = None
+    curr = start
+    while curr != start or prev is None:
         yield curr
         new = get_next_location(grid, curr, prev)
         prev = curr
@@ -66,13 +78,23 @@ def get_path(grid):
     yield curr
 
 
+def print_grid(height, width, pipes):
+    for y in range(height):
+        for x in range(width):
+            if (y, x) in pipes:
+                print("P", end="")
+            else:
+                print(".", end="")
+        print()
+
+
 def main():
     grid = get_grid(inp)
+    start = find_start(grid)
+    grid[start[0]][start[1]] = "F"
 
-    n = 0
-    for element in get_path(grid):
-        n += 1
+    pipes = [element for element in get_path(grid, start)]
 
-    print(max(n // 2, (n+1) // 2))  # I.e. 8 -> 4, 9 -> 5, 10 -> 5, etc.
+    print_grid(len(grid), len(grid[0]), pipes)
 
 main()
