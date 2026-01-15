50c321292ce3247c48b1e01b4abcd7123b07ad2b
Alex Nathanail <7809723+adnathanail@users.noreply.github.com>
2026-01-15 11:40:25 +0100

    2023 day 12 part 1 speed up by not returning the actual strings, just the number, in the recursive function

--- 2023/day12/part1.py
+++ 2023/day12/part1.py
@@ -2,6 +2,12 @@ from aocd.models import Puzzle
 
 puzzle = Puzzle(year=2023, day=12)
 inp = puzzle.input_data
+inp = """???.### 1,1,3
+.??..??...?##. 1,1,3
+?#?#?#?#?#?#?#? 1,3,1,6
+????.#...#... 4,1,1
+????.######..#####. 1,6,5
+?###???????? 3,2,1"""
 
 
 def generate_all_possible_strings(
@@ -12,18 +18,18 @@ def generate_all_possible_strings(
     aim: tuple[int, ...],
 ):
     if len(template) == 0:
-        return [""]
+        return 1 if current_tally == aim else 0
     if len(current_tally) > 0 and (
         len(current_tally) > len(aim)
         or current_tally[len(current_tally) - 1] > aim[len(current_tally) - 1]
     ):
-        return []
+        return 0
 
     if template[0] == "?":
         firsts = ["#", "."]
     else:
         firsts = [template[0]]
-    out = []
+    out = 0
     for first in firsts:
         if first == "#":
             if in_group:
@@ -35,12 +41,9 @@ def generate_all_possible_strings(
             new_tally = current_tally
             new_in_group = False
 
-        out += [
-            first + rest
-            for rest in generate_all_possible_strings(
-                template[1:], new_tally, new_in_group, aim=aim
-            )
-        ]
+        out += generate_all_possible_strings(
+            template[1:], new_tally, new_in_group, aim=aim
+        )
     return out
 
 
@@ -57,20 +60,17 @@ def parse_row(row_str: str) -> tuple[str, tuple[int, ...]]:
     return springs, tuple(int(x) for x in tallies_str.split(","))
 
 
-def count_valid_arrangements(springs: str, desired_tallies: tuple[int, ...]) -> int:
-    num_arrangements_this_row = 0
-    for spring_str in generate_all_possible_strings(springs, aim=desired_tallies):
-        if tally_springs(spring_str) == desired_tallies:
-            num_arrangements_this_row += 1
-    return num_arrangements_this_row
-
-
 def main():
     total_arrangemenets = 0
     for row in inp.splitlines():
+        print(row)
         springs, desired_tallies = parse_row(row)
-        total_arrangemenets += count_valid_arrangements(springs, desired_tallies)
-        # total_arrangemenets += count_valid_arrangements("?".join([springs] * 5), desired_tallies * 5)
+        # total_arrangemenets += generate_all_possible_strings(
+        #     springs, aim=desired_tallies
+        # )
+        total_arrangemenets += generate_all_possible_strings(
+            "?".join([springs] * 5), aim=desired_tallies * 5
+        )
     print(total_arrangemenets)
 
 
