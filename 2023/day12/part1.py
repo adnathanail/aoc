91c8894a1fac78e95c9b38e4c2e937a952ba15de
Alex Nathanail <7809723+adnathanail@users.noreply.github.com>
2026-01-15 12:06:24 +0100

    2023 day 12 part 1 reorganise

--- 2023/day12/part1.py
+++ 2023/day12/part1.py
@@ -1,26 +1,40 @@
+from functools import lru_cache
+
 from aocd.models import Puzzle
 
 puzzle = Puzzle(year=2023, day=12)
 inp = puzzle.input_data
-inp = """???.### 1,1,3
-.??..??...?##. 1,1,3
-?#?#?#?#?#?#?#? 1,3,1,6
-????.#...#... 4,1,1
-????.######..#####. 1,6,5"""
+# inp = """???.### 1,1,3
+# .??..??...?##. 1,1,3
+# ?#?#?#?#?#?#?#? 1,3,1,6
+# ????.#...#... 4,1,1
+# ????.######..#####. 1,6,5
+# ?###???????? 3,2,1"""
+inp = "?????????#?# 1,1,7"
 
 
-def generate_all_possible_strings(
+def tally_springs(springs_str: str) -> tuple[int, ...]:
+    return tuple(
+        len(spring_group)
+        for spring_group in springs_str.split(".")
+        if spring_group != ""
+    )
+
+
+def parse_row(row_str: str) -> tuple[str, tuple[int, ...]]:
+    springs, tallies_str = row_str.split(" ")
+    return springs, tuple(int(x) for x in tallies_str.split(","))
+
+
+@lru_cache
+def count_possible_strings(
     template,
     current_tally: tuple[int, ...] = (0,),
     *,
     aim: tuple[int, ...],
 ):
     if len(template) == 0:
-        # print(
-        #     "\t", current_tally, current_tally == aim or current_tally == (aim + (0,))
-        # )
         return 1 if (current_tally == aim or current_tally == (aim + (0,))) else 0
-    # print(template, current_tally)
     if (
         len(current_tally) > 0
         and (
@@ -44,21 +58,8 @@ def generate_all_possible_strings(
         else:
             new_tally = current_tally
 
-        out += generate_all_possible_strings(template[1:], new_tally, aim=aim)
+        out += count_possible_strings(template[1:], new_tally, aim=aim)
     return out
-
-
-def tally_springs(springs_str: str) -> tuple[int, ...]:
-    return tuple(
-        len(spring_group)
-        for spring_group in springs_str.split(".")
-        if spring_group != ""
-    )
-
-
-def parse_row(row_str: str) -> tuple[str, tuple[int, ...]]:
-    springs, tallies_str = row_str.split(" ")
-    return springs, tuple(int(x) for x in tallies_str.split(","))
 
 
 def main():
@@ -66,8 +67,8 @@ def main():
     for row in inp.splitlines():
         print(row)
         springs, desired_tallies = parse_row(row)
-        # arrs_this_row = generate_all_possible_strings(springs, aim=desired_tallies)
-        arrs_this_row = generate_all_possible_strings(
+        # arrs_this_row = count_possible_strings(springs, aim=desired_tallies)
+        arrs_this_row = count_possible_strings(
             "?".join([springs] * 5), aim=desired_tallies * 5
         )
         print(arrs_this_row)
